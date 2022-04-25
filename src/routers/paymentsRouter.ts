ce33b8d070540eb51963eaf81fc4b49b49614ee4
Liaess <yann.melo@gmail.com>
2022-04-25 15:11:53 -0300

    Preparando revisão de código

--- src/routers/paymentsRouter.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import * as paymentsController from '../controllers/paymentsController.js';
-
-const paymentRouter = express.Router();
-paymentRouter.post('/payments', paymentsController.purchases);
-
-export default paymentRouter;
