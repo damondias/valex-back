1c15998a18df88ca5e7f21196e1bdbe8a94ceed3
Liaess <yann.melo@gmail.com>
2022-04-27 17:35:26 -0300

    Preparando revisão de código

--- src/routers/rechargesRouter.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { recharge } from "../controllers/rechargesController.js";
-import validateCompanyMiddleware from "../middlewares/validateCompanyMiddleware.js";
-
-const rechargesRouter = express.Router();
-
-rechargesRouter.post('/recharges',validateCompanyMiddleware, recharge);
-
-export default rechargesRouter;
